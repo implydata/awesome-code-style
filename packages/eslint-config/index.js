00cd182d9d685f522ed22ac83fe5679ec592efb9
John Gozde <john@gozde.ca>
2021-04-28 18:03:33 -0600

    Disable no-this-alias

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -71,6 +71,7 @@ module.exports = {
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-this-alias': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-unsafe-assignment': 'off', // TODO: use?
     '@typescript-eslint/no-unsafe-call': 'off', // TODO: use?
